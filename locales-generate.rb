76a6b8d42f6e4c1c95badfbfeb7d8dc92aa13333
Tin Vo <vtt999@gmail.com>
2022-05-14 14:25:16 +0000

    fixes output for en-GB and en-US

--- locales-generate.rb
+++ locales-generate.rb
@@ -4,7 +4,16 @@ main = {}
 Dir.glob("./cldr-json/cldr-localenames-full/main/**/languages.json").each { |file| main.merge!(JSON.parse(File.read(file))['main']) }
 output = { version: main['en']['identity']['version']['_cldrVersion'], locales: {} }
 main['en']['localeDisplayNames']['languages'].each do |k,v|
-  name = main[k]['localeDisplayNames']['languages'][k] rescue nil
+  name = case k
+  when 'en-GB'
+    v = 'UK English'
+    'British English'
+  when 'en-US'
+    v = 'US English'
+    'American English'
+  else
+    main[k]['localeDisplayNames']['languages'][k] rescue nil
+  end
   next if name.nil?
   output[:locales][k] = { name: name, english_name: v }
 end
